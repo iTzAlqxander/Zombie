e1a32de76faab6a6c0225c3e2f5dee2d95b1ee7b
Alexander Masztak <iTzAlqxander@gmail.com>
2022-10-04 14:23:00 -0500

    Initial Commit

---
+++ Zombie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zombie
+//
+//  Created by Alex Masztak on 10/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
